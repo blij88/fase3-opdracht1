273bfbdd24f0d4a0ee95bcaeb249e0a48e694218
Fenna <Fennahornstra@gmail.com>
2022-01-11 11:33:52 +0100

    bit of cleanup

--- PhoneShop.Business/Repositories/EFRepository.cs
+++ PhoneShop.Business/Repositories/EFRepository.cs
@@ -7,36 +7,5 @@ namespace PhoneShop.Business.Repositories
 {
     internal class EFRepository<T> : IRepository<T> where T : class
     {
-        public Func<SqlDataReader, T> Mapper { set => throw new NotImplementedException(); }
-
-        public void ExecuteNonQuery(SqlCommand command)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<T> ExecuteStoredProc(SqlCommand command, string CountColName = "TotalCount")
-        {
-            throw new NotImplementedException();
-        }
-
-        public void GetDataCount(int count)
-        {
-            throw new NotImplementedException();
-        }
-
-        public T GetRecord(SqlCommand command)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<T> GetRecords(SqlCommand command)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Status(bool IsError, string strErrMsg)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
